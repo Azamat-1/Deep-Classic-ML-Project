1573c4b70d99b695aa6af8a8d2d0b1dcac89c4ff
Azamat-1 <77926152+Azamat-1@users.noreply.github.com>
2024-10-29 20:04:53 +0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -24,27 +24,6 @@ def weighted_rating(x, m, C):
     score = (v / (v + m) * R) + (m / (v + m) * C)
     return score
 
-def recommend_movies(selected_movie, movies, vectorizer, feature_vectors, k=10):
-    movie_titles = movies['title'].tolist()
-    close_matches = get_close_matches(selected_movie, movie_titles, n=1, cutoff=0.5)
-    if len(close_matches) > 0:
-        selected_movie_title = close_matches[0]
-        idx = movies[movies['title'] == selected_movie_title].index[0]
-    else:
-        return pd.DataFrame()
-    vector = feature_vectors[idx]
-    similarities = cosine_similarity(vector, feature_vectors).flatten()
-    similarities[idx] = -1
-    similar_indices = similarities.argsort()[::-1]
-    recommended_movies = movies.iloc[similar_indices].copy()
-    C = movies['averageRating'].mean()
-    m = movies['numVotes'].quantile(0.70)
-    recommended_movies = recommended_movies[recommended_movies['numVotes'] >= m]
-    recommended_movies['score'] = recommended_movies.apply(weighted_rating, axis=1, m=m, C=C)
-    recommended_movies = recommended_movies.sort_values('score', ascending=False)
-    recommended_movies = recommended_movies.head(k)
-    return recommended_movies
-
 def recommend_by_genres(selected_genres, movies, k=10):
     C = movies['averageRating'].mean()
     m = movies['numVotes'].quantile(0.70)
